1a8017863852d56e10b27af947118f358bb23cb5
Voskanyan Anton <bockahrh@mail.ru>
2021-01-19 22:32:47 +0300

    add tab

---
+++ src/components/Tab.vue
@@ -0,0 +1,21 @@
+<template>
+<div v-show="selected"><slot></slot></div>
+</template>
+
+<script>
+export default {
+    name: 'Tab',
+    props: {
+        name: { required: true },
+        selected: { default: false}
+    },
+    data(){
+        return {isActive: false}
+    },
+    computed: {
+        href(){ return '#' + this.name.toLowerCase().replace(/ /g, '-')}
+    },
+  
+}
+</script>
+
