1239a73d672ae52023eb8469f87cc7513096ed6a
Voskanyan Anton <bockahrh@mail.ru>
2021-01-21 13:31:59 +0300

    fix tab (selected -> data (as isActive))

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <Input title="title" placeholder="Placeholder" @change="onChange($event)" errorMessage="error"/>
-    <Radio title="Radio button" @checked="checked($event)" :disabled="true"/>
+    <Input
+      title="title"
+      placeholder="Placeholder"
+      @change="onChange($event)"
+      errorMessage="error"
+    />
+    <Radio title="Radio button" @checked="checked($event)" :disabled="true" />
     <Tabs>
-      <Tab name="Доставка">
+      <Tab name="Доставка" :selected="true">
         <div class="form1">
-            <Input title="ФИО" placeholder="Только кирилица"/>
-            <Input title="Телефон" placeholder="+7 (___) ___-__-__"/>
-            <Input title="Адрес доставки" placeholder="Город, улица, дом"/>
-            <Input title="Комментарий" textarea/>
-            <!-- <div>ertetr</div>
+          <Input title="ФИО" placeholder="Только кирилица" />
+          <Input title="Телефон" placeholder="+7 (___) ___-__-__" />
+          <Input title="Адрес доставки" placeholder="Город, улица, дом" />
+          <Input title="Комментарий" textarea />
+          <!-- <div>ertetr</div>
             <div>dfgdfg</div>
             <div>xvxvxcv</div>
             <div>ertert</div> -->
-            <Button >Отправить</Button> 
+          <Button>Отправить</Button>
         </div>
       </Tab>
       <Tab name="Самовывоз">
@@ -24,80 +29,78 @@
 </template>
 
 <script>
-import Button from './components/Button.vue';
-import Radio from './components/Radio.vue';
-import Tabs from './components/Tabs.vue';
-import Tab from './components/Tab.vue';
-import Input from './components/Input.vue';
-
+import Button from "./components/Button.vue";
+import Radio from "./components/Radio.vue";
+import Tabs from "./components/Tabs.vue";
+import Tab from "./components/Tab.vue";
+import Input from "./components/Input.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Input, Button, Radio, Tabs, Tab
+    Input,
+    Button,
+    Radio,
+    Tabs,
+    Tab,
   },
-  data:()=>{
+  data: () => {
     return {
-      text:''
-    }
+      text: "",
+    };
   },
-  methods:{
-    onChange(event){
-      console.log(event)
-      this.text=event;
+  methods: {
+    onChange(event) {
+      console.log(event);
+      this.text = event;
     },
-    checked(event){
-      console.log(event)
-    }
-  }
-}
+    checked(event) {
+      console.log(event);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-:root{
-  --btn-normal-bg:  rgb(0,150,218);
-  --btn-clr:  white;
-  --btn-hover-bg:  rgb(0,129,188);
-  --btn-pressed-bg:  rgb(1,93,135);
-  --btn-disabled-bg:  rgb(237,238,239);
-  --btn-disabled-clr:  rgb(184,190,216);
-  --rad-normal: rgb(219,223,240);
+:root {
+  --btn-normal-bg: rgb(0, 150, 218);
+  --btn-clr: white;
+  --btn-hover-bg: rgb(0, 129, 188);
+  --btn-pressed-bg: rgb(1, 93, 135);
+  --btn-disabled-bg: rgb(237, 238, 239);
+  --btn-disabled-clr: rgb(184, 190, 216);
+  --rad-normal: rgb(219, 223, 240);
   --rad-hover: rgb(6, 152, 219);
   --rad-selected: rgb(6, 152, 219);
-  --rad-disabled:  rgb(237,238,239);
+  --rad-disabled: rgb(237, 238, 239);
   --tabs-current-bg: white;
-  --tabs-passive-bg:  rgb(237,238,239);
-  --tab-active-clr:rgb(0,150,218);
-  
-
+  --tabs-passive-bg: rgb(237, 238, 239);
+  --tab-active-clr: rgb(0, 150, 218);
 }
 #app {
- font-family: Montserrat, Arial, Helvetica, sans-serif;
- box-sizing: border-box;
- font-size: 14px;
- background-color: #f6f6f6;
+  font-family: Montserrat, Arial, Helvetica, sans-serif;
+  box-sizing: border-box;
+  font-size: 14px;
+  background-color: #f6f6f6;
 }
-Input{
+Input {
   width: 100%;
 }
-.form1{
+.form1 {
   display: grid;
   grid-gap: 16px;
   grid-template-columns: 1fr 1fr;
-  grid-template-areas: "fio tel"
-                       "adr adr"
-                       "com com"
-                       "com com";
-  & > .input:nth-child(3){
+  grid-template-areas:
+    "fio tel"
+    "adr adr"
+    "com com"
+    "com com";
+  & > .input:nth-child(3) {
     grid-area: adr;
   }
-  & > .input:nth-child(4){
+  & > .input:nth-child(4) {
     grid-area: com;
     height: 10rem;
   }
-
-  
-      
 }
-
 </style>
--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,50 +1,53 @@
 <template>
-    <div>
-        <nav class="tabs">
-              <ul class="tabs__headerlist">
-                <li v-for="(tab,idx) in tabs" :key="'tab'+idx" @click="select(tab)" :class="{active: tab.name===active}">
-                    <a  >{{ tab.name }}</a>
-                </li>
-              </ul>
-        </nav>
-        <div class="tabs__details">
-            <slot></slot>
-        </div>
+  <div>
+    <nav class="tabs">
+      <ul class="tabs__headerlist">
+        <li
+          v-for="(tab, idx) in tabs"
+          :key="'tab' + idx"
+          @click="select(tab)"
+          :class="{ active: tab.name === active }"
+        >
+          <a>{{ tab.name }}</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="tabs__details">
+      <slot></slot>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Tabs',
-    props: {
+  name: "Tabs",
+  props: {},
+  data() {
+    return { tabs: [], active: "" };
+  },
+  mounted() {
+    this.tabs = this.$children;
+    this.active = this.tabs[0].name;
+    // this.$children.forEach(e=>e.selected='false');
+    this.tabs[0].isActibe = "true";
+  },
+  methods: {
+    select(selectedTab) {
+      console.log(selectedTab.name);
+      this.active = selectedTab.name;
+      this.tabs.forEach((tab) => {
+        tab.isActive = tab.name === selectedTab.name;
+      });
     },
-    data() {
-        return {tabs: [], active:'' };
-    },
-    mounted() {
-        this.tabs = this.$children;
-        this.active=this.tabs[0].name;
-        // this.$children.forEach(e=>e.selected='false');
-        this.tabs[0].selected="true";
-    },
-    methods: {
-        select(selectedTab) {
-          console.log(selectedTab.name)
-          this.active=selectedTab.name;
-            this.tabs.forEach(tab => {
-                tab.selected = (tab.name === selectedTab.name);
-            });
-        }
-    }
-  }
-
+  },
+};
 </script>
 
 <style lang="scss" scoped>
- $--tabs-active-bg: white;
- $--tabs-passive-bg:  rgb(237,238,239);
- $--tab-active-clr:rgb(0,150,218);
-ul{
+$--tabs-active-bg: white;
+$--tabs-passive-bg: rgb(237, 238, 239);
+$--tab-active-clr: rgb(0, 150, 218);
+ul {
   display: flex;
 }
 li {
@@ -61,47 +64,48 @@ nav a {
   display: inline-block;
   margin: 0 18px;
   padding: 30px 20px;
-  border-radius: 3px 3px 0 0; 
-  &::before,&::after{
+  border-radius: 3px 3px 0 0;
+  &::before,
+  &::after {
     content: "";
     position: absolute;
     top: 0;
-    
+
     height: 100%;
     background-color: $--tabs-passive-bg;
   }
-  &::before{
+  &::before {
     border-radius: 3px 0 0 0;
-    left: -13px; 
+    left: -13px;
     width: 32px;
   }
-  &::after{
+  &::after {
     border-radius: 0 3px 0 0;
     transform: skew(24deg);
     width: 40px;
-    right: -21px; 
+    right: -21px;
     border-right: 1px solid #777;
-    z-index: 1; 
+    z-index: 1;
   }
-  &:hover{
-    color:$--tab-active-clr
+  &:hover {
+    color: $--tab-active-clr;
   }
 }
 
 nav li.active {
-  & a{
+  & a {
     color: $--tab-active-clr;
     background-color: $--tabs-active-bg;
   }
   & a:before {
-    z-index: 1; 
-    background-color: $--tabs-active-bg;/* overlap prev element */
+    z-index: 1;
+    background-color: $--tabs-active-bg; /* overlap prev element */
   }
   & a:after {
     background-color: $--tabs-active-bg;
   }
 }
-.tabs__details{
+.tabs__details {
   background-color: $--tabs-active-bg;
   margin: -14px 29px;
   padding: 29px 32px;
