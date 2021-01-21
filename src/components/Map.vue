a3f4221d6cab9fda93318f244e564b31e93c39e0
Voskanyan Anton <bockahrh@mail.ru>
2021-01-22 01:12:05 +0300

    map run!

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,15 @@
 <template>
-  <yandex-map
-    :settings="settings"
-    :coords="coords"
-    :zoom="10"
-    style="width: 100%; height: 100%"
-  >
-    <ymap-marker :coords="coords" marker-id="123" hint-content="some hint" />
-  </yandex-map>
+  <div class="wrap">
+    <yandex-map
+      :settings="settings"
+      :coords="coords"
+      :zoom="10"
+      style="width: 100%; height: 100%"
+      :icon="markerIcon"
+    >
+      <ymap-marker :coords="coords" marker-id="123" hint-content="some hint" />
+    </yandex-map>
+  </div>
 </template>
 
 <script>
@@ -18,12 +21,22 @@ export default {
   data() {
     return {
       settings: {
-        apiKey: "68803c60-89f0-48f9-a7a4-b9bb329ecc85",
+        apiKey: "9b27ecf5-fac6-419d-b8f5-184a9818db91",
         lang: "ru_RU",
         coordorder: "latlong",
         version: "2.1",
       },
       coords: [54.82896654088406, 39.831893822753904],
+      markerIcon: {
+        layout: "default#imageWithContent",
+        imageHref: "https://image.flaticon.com/icons/png/512/33/33447.png",
+        imageSize: [43, 43],
+        imageOffset: [0, 0],
+        content: "123 v12",
+        contentOffset: [0, 15],
+        contentLayout:
+          '<div style="background: red; width: 50px; color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>',
+      },
     };
   },
   mounted() {
@@ -41,6 +54,10 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.wrap {
+  width: 100%;
+  height: 400px;
+}
 </style>
 
 
