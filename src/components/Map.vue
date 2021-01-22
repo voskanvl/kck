0686d8368546f0f8f8c3f12f80e62afd9a719d76
Voskanyan Anton <bockahrh@mail.ru>
2021-01-22 04:32:20 +0300

    set coords

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,14 +2,23 @@
   <div class="wrap">
     <yandex-map
       :settings="settings"
-      :coords="coords[0]"
-      :zoom="14"
+      :coords="coords[0].coords"
+      :zoom="11"
       style="width: 100%; height: 100%"
-      :icon="markerIcon"
       :scroll-zoom="false"
     >
-      <ymap-marker :coords="coords[1]" marker-id="1" hint-content="some hint" />
-      <ymap-marker :coords="coords[0]" marker-id="2" hint-content="some hint" />
+      <ymap-marker
+        :coords="coords[0].coords"
+        marker-id="1"
+        :hint-content="coords[0].adr"
+        :icon="markerIcon"
+      />
+      <ymap-marker
+        :coords="coords[1].coords"
+        marker-id="2"
+        :hint-content="coords[1].adr"
+        :icon="markerIcon"
+      />
     </yandex-map>
   </div>
 </template>
@@ -29,14 +38,24 @@ export default {
         version: "2.1",
       },
       coords: [
-        [54.82896654088406, 39.831893822753904],
-        [54.82496654088406, 39.831893822753904],
+        {
+          adr: "Песчаная улица, дом 13",
+          coords: [55.801131, 37.508167],
+        },
+        {
+          adr: "Подсосенский переулок, 11",
+          coords: [55.757556, 37.651592],
+        },
       ],
       markerIcon: {
-        layout: "default#image",
-        imageHref: "src/assets/klipartz.com.png",
-        imageSize: [43, 55],
-        imageOffset: [-22, -55],
+        layout: "default#imageWithContent",
+        imageHref: "klipartz.com.png",
+        imageSize: [43, 43],
+        imageOffset: [0, 0],
+        content: "",
+        contentOffset: [10, 15],
+        contentLayout:
+          '<div style="background: red; width: 50px; color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>',
       },
       children: this.$children,
     };
