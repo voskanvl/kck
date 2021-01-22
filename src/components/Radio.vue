6790dce6d8e389673bd3f5abff88955431b22c5b
Voskanyan Anton <bockahrh@mail.ru>
2021-01-22 03:42:46 +0300

    fix Radio

--- src/components/Radio.vue
+++ src/components/Radio.vue
@@ -1,99 +1,101 @@
 <template>
-  <div class="radio" :disabled="disabled">
-    <div class="check" :class='{disabled, checked}' @click="clickHandle"><div class="dot" v-show="checked"></div></div>
+  <div class="radio" :disabled="disabled" @click="clickHandle">
+    <div class="check" :class="{ disabled, checked }">
+      <div class="dot" v-show="checked"></div>
+    </div>
     <div class="title">
-      {{title}}
+      {{ title }}
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Radio',
+  name: "Radio",
   props: {
     selected: Boolean,
     disabled: Boolean,
-    title:String
+    title: String,
   },
-  data(){
+  data() {
     return {
-      checked:false
-    }
+      checked: false,
+    };
   },
-  methods:{
-    clickHandle(){
-      if (!this.disabled ){
-        this.checked=!this.checked;
-        this.$emit('checked', this.checked)
+  methods: {
+    clickHandle() {
+      if (!this.disabled) {
+        this.checked = !this.checked;
+        this.$emit("checked", this.checked);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-$--rad-normal: rgb(219,223,240);
-  $--rad-hover: rgb(6, 152, 219);
-  $--rad-selected: rgb(6, 152, 219);
-  $--rad-disabled:  rgb(237,238,239);
-.radio{
+$--rad-normal: rgb(219, 223, 240);
+$--rad-hover: rgb(6, 152, 219);
+$--rad-selected: rgb(6, 152, 219);
+$--rad-disabled: rgb(237, 238, 239);
+.radio {
   display: flex;
   position: relative;
   justify-content: stretch;
-  align-items:center;
+  align-items: center;
   &:hover {
-    & > .check{
+    & > .check {
       border: 1px solid $--rad-hover;
     }
-    & > .title{
+    & > .title {
       color: $--rad-hover;
     }
-    &  .dot{
+    & .dot {
       background-color: $--rad-hover;
       border: 1px solid $--rad-hover;
     }
   }
 }
-.radio[disabled]{
-  & .title{
-    color:$--rad-disabled;
+.radio[disabled] {
+  & .title {
+    color: $--rad-disabled;
   }
-  & .dot{
+  & .dot {
     background-color: $--rad-disabled;
     border: 1px solid $--rad-disabled;
   }
-  & .check{
-      border: 1px solid $--rad-disabled;
+  & .check {
+    border: 1px solid $--rad-disabled;
+    background-color: $--rad-disabled;
+  }
+
+  &:hover {
+    & .dot {
       background-color: $--rad-disabled;
-    }
-  
-  &:hover{
-    & .dot{
-      background-color: $--rad-disabled;
       border: 1px solid $--rad-disabled;
     }
-    & .check{
+    & .check {
       border: 1px solid $--rad-disabled;
     }
   }
 }
-.check{
+.check {
   height: 20px;
   width: 20px;
 
   border-radius: 50%;
   border: 1px solid $--rad-normal;
-  &.checked:not(.disabled){
+  &.checked:not(.disabled) {
     border: 1px solid $--rad-selected;
-    & .dot{
+    & .dot {
       background-color: $--rad-selected;
       border: 1px solid $--rad-selected;
     }
   }
 }
-.dot{
-   position: relative;
+.dot {
+  position: relative;
   top: 50%;
   left: 50%;
   height: 6px;
@@ -104,10 +106,7 @@ $--rad-normal: rgb(219,223,240);
   background-color: $--rad-normal;
   // opacity: 0;
 }
-.title{
+.title {
   margin-left: 0.5rem;
 }
-
-
-
 </style>
