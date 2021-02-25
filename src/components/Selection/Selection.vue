c20d6199b15760c4e4533b779a1d23bd1362b19e
Anton Voskanyan <voskanvl@gmail.com>
2021-02-25 12:54:16 +0300

    get geomarks from json

--- src/components/Selection/Selection.vue
+++ src/components/Selection/Selection.vue
@@ -6,32 +6,13 @@
       :selectPoint="selectPoint"
     >
       <Radio
-        title="Пункт выдачи заказов Подсосенский переулок, 11"
-        @checked="onCheck($event, 'Подсосенский переулок, 11')"
-        :selected="isSelected('Подсосенский переулок, 11')"
-        name="Подсосенский"
-        adr="Москва, Подсосенский переулок, 11"
-      />
-      <Radio
-        title="Пункт выдачи заказов Песчаная улица, дом 13"
-        :selected="isSelected('Песчаная улица, дом 13')"
-        @checked="onCheck($event, 'Песчаная улица, дом 13')"
-        name="Песчаная"
-        adr="Москва, Песчаная улица, дом 13"
-      />
-      <Radio
-        title="Пункт выдачи заказов Новый"
-        @checked="onCheck($event, 'Новый, 11')"
-        :selected="isSelected('Новый, 11')"
-        name="Новый"
-        adr="Москва, Новый, 11"
-      />
-      <Radio
-        title="Пункт выдачи заказов Старый"
-        @checked="onCheck($event, 'Старый, 10')"
-        :selected="isSelected('Старый, 10')"
-        name="Старый"
-        adr="Москва, Старый, 10"
+        v-for="i of radios"
+        :title="i.title"
+        :selected="isSelected(i.adr)"
+        :name="i.name"
+        :adr="i.adress"
+        :key="i.name"
+        @checked="onCheck($event, i.adr)"
       />
     </Map>
     <div class="delivery__sent">
@@ -44,6 +25,7 @@
 import Button from "../Button/Button";
 import Map from "../Map_/Map";
 import Radio from "../Radio/Radio";
+import takePoints from "../../takePoints.json";
 
 export default {
   name: "App",
@@ -56,6 +38,7 @@ export default {
   data: () => {
     return {
       selectPoint: "Подсосенский переулок, 11",
+      radios: null,
     };
   },
   methods: {
@@ -65,6 +48,9 @@ export default {
     isSelected(adress) {
       return this.selectPoint === adress;
     },
+  },
+  mounted() {
+    this.radios = takePoints;
   },
 };
 </script>
